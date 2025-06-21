c56a3c53d934f8e2e7848131268c488d11dfaf20
mhdshaikh <mhdshaikh20403@gmail.com>
2025-06-22 00:20:10 +0300

    Integrating GORM library to database interactions

---
+++ backend/models/employees.go
@@ -0,0 +1,9 @@
+package models
+
+type Employee struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Position  string `json:"position"`
+}
